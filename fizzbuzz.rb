401b2a2881acdff5832cf343b46e43ff1a8a4912
alwaysmaria1 <alwaysmaria1@gmail.com>
2018-07-20 13:28:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,19 @@
 def fizz_buzz
-num=gets.strip.to_i
-if (( num % 3==0 )&& (num % 5==0 ) )
-  puts "fizzbuzz"
-  elsif num % 3==0 
-  puts "fizz"
-  elsif num % 5==0
-  puts "buzz"
-  else 
-  puts num 
+  num=1 
+  while num<201
+    if (( num % 3==0 )&& (num % 5==0 ) )
+     puts "fizzbuzz"
+    elsif num % 3==0 
+     puts "fizz"
+    elsif num % 5==0
+      puts "buzz"
+    else 
+     puts num 
+     end
+     num+=1 
   end 
 end 
 fizz_buzz
-fizz_buzz
+
 
 
